883c8dccd7af382b1df2288a80fba31137bd0125
Joshua Savage <177585889+SkyBlueNomad@users.noreply.github.com>
2025-10-27 14:40:40 +0100

    completed computer choice function

---
+++ RPS.js
@@ -0,0 +1,12 @@
+// Get choice from Computer
+function getcomputerChoice(max) {
+    let computerchoice = Math.floor(Math.random() * max);
+    return computerchoice;
+}
+
+// Get choice from human
+// Check Scores
+// Play Round
+// Calculate Scores
+// Play until 5 rounds
+// Declare Winner
