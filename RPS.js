40c25295397fdc21fb60db97cfb6d262791fed52
Joshua Savage <177585889+SkyBlueNomad@users.noreply.github.com>
2025-10-28 14:02:47 +0100

    added computer choice and human choice functions

--- RPS.js
+++ RPS.js
@@ -1,10 +1,34 @@
 // Get choice from Computer
 function getcomputerChoice(max) {
-    let computerchoice = Math.floor(Math.random() * max);
-    return computerchoice;
+    let computerChoice = Math.floor(Math.random() * max);
+
+    if (computerChoice === 0) {
+        return "Rock";
+    } else if (computerChoice === 1) {
+        return "Paper";
+    } else if (computerChoice === 2) {
+        return "Scissors";
+    }
 }
 
+// to debug computer choice function - console.log(getcomputerChoice(3));
 // Get choice from human
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+    humanChoice = humanChoice.toLocaleLowerCase();
+
+    if  (humanChoice === "rock") {
+        return "rock";
+    } else if (humanChoice === "paper") {
+        return "paper";
+    } else if (humanChoice === "scissors") {
+        return "scissors";
+    } else {
+        return "Error";
+    }
+}
+
+console.log(getHumanChoice());
 // Check Scores
 // Play Round
 // Calculate Scores
